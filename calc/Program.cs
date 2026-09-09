74642d3e4947b482e2ffdb8b6d9767e6f7f9e864
john sharik <103885785+skr1lxfn@users.noreply.github.com>
2026-09-09 13:27:13 +0500

    calc.cs

--- calc/Program.cs
+++ calc/Program.cs
@@ -1,5 +1,28 @@
-﻿Console.WriteLine("Введите первое значение");
-Console.ReadLine();
-Console.WriteLine("Введите второе значение");
-Console.ReadLine();
+using System;
+while (true)
+{
+    Console.WriteLine("Введите первое значение");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите знак (+, -, *, /): ");
+    string operation = Console.ReadLine();
+    Console.WriteLine("Введите второе значение");
+    double b = Convert.ToDouble(Console.ReadLine());
+    if (operation == "+")
+    {
+        Console.WriteLine($"Ответ:{a + b}");
+    }
+    else if (operation == "-")
+    {
+        Console.WriteLine($"Ответ:{a - b}");
+    }
+    else if (operation == "*")
+    {
+        Console.WriteLine($"Ответ:{a * b}");
+    }
+    else if (operation == "/")
+    {
+        Console.WriteLine($"Ответ:{a / b}");
+    }
+}
+
 
